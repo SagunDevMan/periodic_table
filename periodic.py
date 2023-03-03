c7af88ac8f1365ae01c50ca625a120fec0cff568
Sagun <125622397+SagunDevMan@users.noreply.github.com>
2023-03-03 15:45:18 -0600

    Periodic.py improved with better comments

--- periodic.py
+++ periodic.py
@@ -24,41 +24,68 @@
 import random
 from table import periodic_table
 
-exit = 0
+# Variables
+exit_value = 0
+answer = ' '
+score = 0
+question = 0
+
 # Input
-while exit == 0:
+while exit_value == 0:
     print("-" * 50)
     print("Welcome to Sagun's Periodic Table Question")
     print("-" * 50)
     print("""1. Start\n2. Instructions\n3. Exit""")
     menu_selection = int(input("Choose your menu selection (1 - 3): "))
-    print("-" * 50)
 
 # Procress
+    # If the menu selection is 1, then start the program
     if menu_selection == 1:
-        atomic_number = random.randint(1, 118)
-        element = periodic_table(atomic_number)
-        print("\n")
-        print("What is the element with an atomic number of " + str(atomic_number) + "?")
-        answer = input("Type your answer here: ").lower()
-        print("\n\n")
-        if answer == element:
-            print("Your answer is correct!")
-        else:
-            print("You got it wrong! The correct answer is", element.capitalize())
-        print("\n")
+        while answer != 'x':
+            atomic_number = random.randint(1, 118)
+            element = periodic_table(atomic_number)
+            print("-" * 50)
+            print("What is the element with an atomic number of " + str(atomic_number) + "?")
+            answer = input("Type your answer here, (type x for exit): ").lower()
+            print("\n")
+            
+            # Exits the questions if user types in 'x'
+            if answer == 'x':
+                print("You got", score, "out of", question, "right!")
+                print("Exiting...")
+            # elif the answer is equal to the element, say you are correct and give them +1 score
+            elif answer == element:
+                question += 1
+                score += 1
+                print("Your answer is correct! You have", score, "out of", question, "right!")
+            # Else the user it's wrong with the correct answer
+            else:
+                question += 1
+                print("You got it wrong! The correct answer is", element.capitalize())
+                print("You have", score, "out of", question, "right!\n\nKeep going you got this!")
+            print("\n")
         
         
 
 
-        
+    # If the menu selection is 2, then give instructions    
     elif menu_selection == 2:
-        print("You chose 2")
+        print("""
+- In order to use this program, type the number 1 which will select start.
+- The program will give you an atomic number and you have to answer with the correct element.
+- In order to answer, you have to type it in!
+- If you would like to leave the program afterwards, just type in the letter x. 
+
+""")
 
 
 
-        
+    # If menu selection is 3, exit the program   
+    elif menu_selection == 3:
+        exit_value = 1
+
+    # Else, print in valid number
     else:
-        exit = 1
+        print("Invalid number!")
     
     
